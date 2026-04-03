5397ab27ea2aff1023febf73b649f43cdd0c4338
AlYousif-dev <yousifbal4@gmail.com>
2026-04-03 13:17:20 -0400

    Add: maximum-product-difference-between-two-pairs - submission-7

---
+++ Data Structures & Algorithms/maximum-product-difference-between-two-pairs/submission-7.py
@@ -0,0 +1,27 @@
+import random
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        
+        def quick_sort(arr):
+            if len(arr) <= 1:
+                return arr
+            else:
+                pivot = random.choice(arr)
+            
+            low = []
+            mid = []
+            high = []
+
+            for num in arr:
+                if num > pivot:
+                    high.append(num)
+                elif num < pivot:
+                    low.append(num)
+                else:
+                    mid.append(num)
+
+            return quick_sort(low) + mid + quick_sort(high)
+        
+        
+        nums = quick_sort(nums)
+        return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
