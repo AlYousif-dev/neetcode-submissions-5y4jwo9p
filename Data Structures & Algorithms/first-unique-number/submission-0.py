54f3e721f115076adf353a2fee655ea87d55c125
AlYousif-dev <yousifbal4@gmail.com>
2026-04-07 08:58:34 -0400

    Add: first-unique-number - submission-0

---
+++ Data Structures & Algorithms/first-unique-number/submission-0.py
@@ -0,0 +1,26 @@
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def showFirstUnique(self) -> int:
+
+        for i in range(len(self.nums)):
+            val = self.nums[i]
+            check = self.nums.copy()
+            check.remove(val)
+            if val not in check:
+                return val
+            elif len(check) <= 1:
+                return -1
+        return -1
+        
+
+    def add(self, value: int) -> None:
+        self.nums.append(value)
+
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
