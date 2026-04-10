d6ac8b41966c20394be51cdf66805ec35c16ea16
AlYousif-dev <yousifbal4@gmail.com>
2026-04-10 09:21:48 -0400

    Add: length-of-last-word - submission-3

---
+++ Data Structures & Algorithms/length-of-last-word/submission-3.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        s = s.title()
+        s = s.strip()
+        count = 1 
+        for c in s:
+            if c.isupper():
+                count = 0
+            count += 1
+        return count
